48ec7cf16ffea96664f3561a46182050b4b0db11
Iskovskikh Alexey <iskovskikh-av@localhost>
2026-05-22 11:56:50 +0300

    upd

--- app/application/api/v1/auth/handlers.py
+++ app/application/api/v1/auth/handlers.py
@@ -3,15 +3,16 @@
 
 import logging
 import pprint
-
+from typing import Annotated
 from colorama import Style, Fore
 from fastapi import APIRouter, Depends
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer, OAuth2PasswordBearer
 
+from application.api.v1.dependencies import ContainerDependency, RequireRole
 from application.api.v1.auth.schemas import ProfileResponseSchema
 from application.api.v1.dependencies import ContainerDependency, require_role
 from application.api.v1.secret.schemas import EditSecretRequestSchema
-from application.services.keycloak import KeycloakService
+from application.services.keycloak import KeycloakService, User
 from settings.config import Config
 from application.services.keycloak import KeycloakService, User
 
@@ -21,13 +22,18 @@ router = APIRouter(prefix="/auth")
 
 logger = logging.getLogger(__name__)
 
-
 @router.get(
     path="/profile",
 )
 async def get_profile_handler(
     container: ContainerDependency,
-    credentials: HTTPAuthorizationCredentials = Depends(security),
+    user: Annotated[User, Depends(RequireRole(required_role='any'))],
 ) -> ProfileResponseSchema:
 
-    return ProfileResponseSchema()
+    config = container.resolve(Config)
+
+    return ProfileResponseSchema(
+        username = user.username,
+        roles = user.roles,
+        permissions = [],
+    )
